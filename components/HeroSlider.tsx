ccd82227a3d4f7ad0c764bed11d4305a0b88dc67
joifzeio <nathan.theodros.2nd3@gmail.com>
2026-01-17 01:39:14 +0100

    chore: remove city selector from hero section

--- components/HeroSlider.tsx
+++ components/HeroSlider.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Crosshair from './Crosshair';
 import { useLanguage } from '../contexts/LanguageContext';
 
-const CITIES = ['PARIS', 'RENNES', 'NANCY', 'NICE'];
 
 const HeroSlider: React.FC = () => {
   const { t } = useLanguage();
@@ -27,22 +26,7 @@ const HeroSlider: React.FC = () => {
         </h1>
       </div>
 
-      {/* City Selector Overlay */}
-      <div className="absolute bottom-[15%] left-0 w-full z-20 flex justify-center px-4">
-        <div className="flex flex-wrap justify-center gap-4 md:gap-8">
-          {CITIES.map((city) => (
-            <button
-              key={city}
-              className="font-display text-2xl md:text-5xl font-bold uppercase text-transparent tracking-tight transition-all duration-300 hover:text-action-orange hover:scale-110"
-              style={{ WebkitTextStroke: '1px white' }}
-              onMouseEnter={(e) => (e.currentTarget.style.webkitTextStroke = '1px #FF4500')}
-              onMouseLeave={(e) => (e.currentTarget.style.webkitTextStroke = '1px white')}
-            >
-              {city}
-            </button>
-          ))}
-        </div>
-      </div>
+
 
       <Crosshair position="bottom-right" className="m-4 md:m-12" />
       <Crosshair position="bottom-left" className="m-4 md:m-12" />
